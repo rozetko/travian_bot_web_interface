992bc04436b3064dac181e216a15c9a6570198dc
rozetko <rozetko09@gmail.com>
2015-03-07 12:56:57 -0500

    Deleted bash-files calls

--- ajax.php
+++ ajax.php
@@ -49,89 +49,88 @@
 		return $a['village'] > $b['village'];
 	}
 
-	if (isset($_GET["bot"])) {
-		$filename = "bot/salik.json";
+	$filename = "bot/salik.json";
 
-		if (isset($_GET["start"])) {
-			exec("sh start_bot.sh");
-		}
+	if (isset($_GET["start"])) {
+		exec("python bot/bot.py salik.json start");
+	}
 
-		if (isset($_GET["stop"])) {
-			exec("sh stop_bot.sh");
-		}
+	if (isset($_GET["stop"])) {
+		exec("python bot/bot.py salik.json stop");
+	}
 
-		if (isset($_GET["pause"])) {
-			$config = getConfig($filename);
-			$config["pause"] = $_GET["pause"];
+	if (isset($_GET["pause"])) {
+		$config = getConfig($filename);
+		$config["pause"] = $_GET["pause"];
 
-			saveConfig($filename, $config);
-		}
+		saveConfig($filename, $config);
+	}
 
-		if (isset($_GET["update"])) {
-			$return = array();
+	if (isset($_GET["update"])) {
+		$return = array();
 
-			$configString = file_get_contents($filename);
-			$config = json_decode($configString, true);
+		$configString = file_get_contents($filename);
+		$config = json_decode($configString, true);
 
-			$log = nl2br(shell_exec("exec tail -n 20 bot/debug.log"));
+		$log = nl2br(shell_exec("exec tail -n 20 bot/debug.log"));
 
-			if (file_exists("/tmp/bot_salik.pid")) {
-				if ($config["pause"]) {
-					$return["status"] = "-1";
-				}
-				else {
-					$return["status"] = "1";
-				}
+		if (file_exists("/tmp/bot_salik.pid")) {
+			if ($config["pause"]) {
+				$return["status"] = "-1";
 			}
 			else {
-				$return["status"] = "0";
+				$return["status"] = "1";
 			}
-
-			$return['log_hash'] = crc32($log);
-			$return['log'] = $log;
-
-			$return['config_hash'] = crc32($configString);
-
-			echo json_encode($return);
+		}
+		else {
+			$return["status"] = "0";
 		}
 
-		if (isset($_GET["save_config"])) {			
-			$config = getConfig($filename);
+		$return['log_hash'] = crc32($log);
+		$return['log'] = $log;
 
-			$config['login']['server'] = $_POST['server'];
-			$config['login']['username'] = $_POST['username'];
-			$config['login']['password'] = $_POST['password'];
-			$config['adventures']['enable'] = $_POST['adventures']? 1: 0;
-			$config['build']['enable'] = $_POST['build']? 1: 0;
+		$return['config_hash'] = crc32($configString);
 
-			$config['build']['buildingList'] = array();
-		
-			foreach ($_POST['village'] as $i => $village) {
-				$config['build']['buildingList'][] = array(
-					'village' => (int) $village,
-					'fieldId' => (int) $_POST['field_id'][$i],
-					'to' => (int) $_POST['to'][$i],
-					'skip' => $_POST['skip'][$i]? 1: 0,
-					'wood' => $_POST['wood'][$i]? 1: 0,
-					'clay' => $_POST['clay'][$i]? 1: 0,
-					'iron' => $_POST['iron'][$i]? 1: 0,
-					'crop' => $_POST['crop'][$i]? 1: 0,
-				);
-			}
+		echo json_encode($return);
+	}
 
-			usort($config['build']['buildingList'], 'customSort');
+	if (isset($_GET["save_config"])) {			
+		$config = getConfig($filename);
 
-			saveConfig($filename, $config);
+		$config['login']['server'] = $_POST['server'];
+		$config['login']['username'] = $_POST['username'];
+		$config['login']['password'] = $_POST['password'];
+		$config['adventures']['enable'] = $_POST['adventures']? 1: 0;
+		$config['build']['enable'] = $_POST['build']? 1: 0;
+		$config['build']['delay'] = $_POST['build_delay'];
+
+		$config['build']['buildingList'] = array();
+	
+		foreach ($_POST['village'] as $i => $village) {
+			$config['build']['buildingList'][] = array(
+				'village' => (int) $village,
+				'fieldId' => (int) $_POST['field_id'][$i],
+				'to' => (int) $_POST['to'][$i],
+				'skip' => $_POST['skip'][$i]? 1: 0,
+				'wood' => isset($_POST['wood'][$i])? 1: 0,
+				'clay' => isset($_POST['clay'][$i])? 1: 0,
+				'iron' => isset($_POST['iron'][$i])? 1: 0,
+				'crop' => isset($_POST['crop'][$i])? 1: 0,
+			);
 		}
 
-		if (isset($_GET["config"])) {
-			$config = getConfig($filename);
+		usort($config['build']['buildingList'], 'customSort');
 
-			echo renderConfig($config);
-		}
+		saveConfig($filename, $config);
+	}
 
-		if (isset($_GET["row"])) {
-			echo renderRow($_GET["row"]);
-		}
+	if (isset($_GET["config"])) {
+		$config = getConfig($filename);
+
+		echo renderConfig($config);
+	}
+
+	if (isset($_GET["row"])) {
+		echo renderRow($_GET["row"]);
 	}
 ?>
